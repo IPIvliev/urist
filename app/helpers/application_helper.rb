e28719b74ab118f947cffad68365fce9251c7bdd
IPIvliev <cccp1945@mail.ru>
2014-11-17 16:14:46 +0300

    Start to change for nnte

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Юридическая компания"
+    base_title = "ООО 'Нижновтеплоэнерго'"
     if page_title.empty?
       base_title
     else
